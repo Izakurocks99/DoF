32f273527090f0c5c92658980bba8e821ac5a640
Isaac Choo <isaacchoo_jl@hotmail.com>
2018-11-11 22:54:26 +0800

    Fixed flipping
    - Edited cardbase to fix flipping

--- Assets/Scripts/Cards/CardBase.cs
+++ Assets/Scripts/Cards/CardBase.cs
@@ -4,22 +4,22 @@ using UnityEngine;
 
 public class CardBase : MonoBehaviour {
 
-    bool flipped = false; //faced down?
+    public bool flipped = false; //faced down?
     bool flipping = false; //faced down?
     float flipHeight; //how high it flips, use force instead?
 
     [SerializeField]
     float flipSpeed; //time taken to flip
-    public float fliptime;
-    public float percentage;
-
-    [SerializeField]
-    Transform startTransform;
+    float fliptime;
+    
     [SerializeField]
     Transform endTransform;
 
+    Quaternion endRotation;
+
     // Use this for initialization
     void Start () {
+        
 	}
 	
 	// Update is called once per frame
@@ -32,26 +32,27 @@ public class CardBase : MonoBehaviour {
 
         if (flipping)
         {
-            fliptime += Time.deltaTime;
-            percentage = fliptime / flipSpeed;
+            fliptime += Time.deltaTime * flipSpeed;
 
-            transform.rotation = Quaternion.Lerp(startTransform.rotation, endTransform.rotation, percentage);
+            transform.rotation = Quaternion.Lerp(transform.rotation, endRotation, fliptime);
 
-            if (percentage >= 1f)
+            if (fliptime >= 1f)
             {
-                transform.rotation = endTransform.rotation;
+                transform.rotation = endRotation;
                 flipping = false;
-                fliptime = 0;
-                percentage = 0;
             }
         }
     }
 
     private void OnMouseOver()
     {
-        if(Input.GetMouseButtonDown(0) && !flipped)
+        if(Input.GetMouseButtonDown(0))
         {
+            endRotation = endTransform.transform.rotation;
             flipping = true;
+            fliptime = 0;
+
+            flipped = !flipped;
         }
     }
 }
